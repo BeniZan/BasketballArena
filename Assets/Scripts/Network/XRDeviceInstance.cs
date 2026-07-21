fa93799a45d548f9136d15475118668b5e09a7aa
Beni <bnayahu@arena-digital.com>
2026-07-21 20:20:32 +0300

    Manual XR init

--- Assets/Scripts/Network/XRDeviceInstance.cs
+++ Assets/Scripts/Network/XRDeviceInstance.cs
@@ -3,11 +3,16 @@ using Meta.XR.MRUtilityKit;
 using Oculus.Interaction;
 using Sirenix.OdinInspector;
 using System;
+using System.Collections;
 using System.Threading.Tasks;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.XR.Management;
 
 public class XRDeviceInstance : SingletonBehaviors.SingletonMono<XRDeviceInstance> {
+    const float RetryConnectXRDelay = 5f;
+    private static WaitForSeconds _waitForRetryConnectXR = new WaitForSeconds(RetryConnectXRDelay);
+
     static public bool ENABLE_MRUK => Application.isEditor;
 	[field: SerializeField] public OVRHand LocalRightHand { get; private set; }
 	[field: SerializeField] public OVRHand LocalLeftHand { get; private set; }
@@ -16,8 +21,10 @@ public class XRDeviceInstance : SingletonBehaviors.SingletonMono<XRDeviceInstanc
     [field: SerializeField] public Transform CenterEyes { get; private set; }
     [SerializeField] MRUK _mruk;
     [SerializeField] EffectMesh _efMesh;
+    CustomLogger _logger;
     protected override void Awake() {
         base.Awake();
+        _logger = new CustomLogger(this, Color.green);
         var enableMRUK = Application.isEditor;
         if(_mruk)
             _mruk.gameObject.SetActive(ENABLE_MRUK);
@@ -33,11 +40,55 @@ public class XRDeviceInstance : SingletonBehaviors.SingletonMono<XRDeviceInstanc
 
     Task<MRUK.LoadDeviceResult> _awaitingRoom;
     void OnEnable() {
+        StartXR();
         if (_mruk) {
             _mruk.ClearScene();
             if(_awaitingRoom == null || _awaitingRoom.IsCompleted)
-            _awaitingRoom = _mruk.LoadSceneFromPrefab(_mruk.SceneSettings.RoomPrefabs[0], true);
+                _awaitingRoom = _mruk.LoadSceneFromPrefab(_mruk.SceneSettings.RoomPrefabs[0], true);
         }
     }
 
+    void StartXR() {
+        StopAllCoroutines();
+       // StartCoroutine(StartXRCoroutine());
+    }
+
+    IEnumerator StartXRCoroutine() {
+        _logger.Log("Initializing XR...");
+
+        var mnger = XRGeneralSettings.Instance.Manager;
+        if (!mnger) {
+            _logger.LogError("XR Manager not found. Ensure XR is properly configured in Project Settings.");
+            yield break;
+        }
+        if (!mnger.activeLoader) {
+            yield return mnger.InitializeLoader();
+        }
+
+        if (!mnger.activeLoader) {
+            _logger.LogError("XR Initialization failed. Ensure your headset is connected."
+                                + "\n Retrying in " + RetryConnectXRDelay + " seconds...");
+            yield return _waitForRetryConnectXR;
+            StartCoroutine(StartXRCoroutine());
+            yield break;
+        }
+        _logger.Log("XR initialized successfully. Starting subsystems..."); 
+        mnger.StartSubsystems(); 
+    }
+    void OnDisable() {
+        StopXR();
+    }
+    void StopXR() {
+        StopAllCoroutines();
+        var mnger = XRGeneralSettings.Instance.Manager;
+        if (mnger && mnger.isInitializationComplete) {
+            _logger.Log("Stopping XR Subsystems...");
+             
+            mnger.StopSubsystems();
+             
+            mnger.DeinitializeLoader();
+
+            _logger.Log("XR shutdown complete.");
+        }
+    }
 }
